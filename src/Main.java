77a183714da61ada408276a91de49c0c05108e1d
Bzaraza <starkandco@icloud.com>
2022-10-21 20:10:03 +0300

    test1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import Package1.MyArea;
+
+public class Main  {
+    public static void main(String[] args) {
+        System.out.println(MyArea.AreaOfCircle(4));
+    }
+}
---
+++ src/Package1/MyArea.java
@@ -0,0 +1,10 @@
+package Package1;
+
+public class MyArea {
+
+    public static double AreaOfCircle(double r){
+        final double PI = 3.14;
+        return Math.pow(r,2)*PI;
+    }
+
+}
